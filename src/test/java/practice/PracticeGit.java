98536848f5b2ebe6ddbeb68e77c3a17b9c8ad419
kc <chaitanya1273@gmail.com>
2023-10-24 17:17:09 +0530

    modified first message x

--- src/test/java/practice/PracticeGit.java
+++ src/test/java/practice/PracticeGit.java
@@ -4,7 +4,7 @@ public class PracticeGit {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("First message from y");
+		System.out.println("Modified first message");
 		
 		System.out.println("Second message from x");
 		
